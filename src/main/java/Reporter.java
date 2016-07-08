3c9bb99f4bd399dbe48d29d8422288382f1e630b
Austin Kuo <auszon3@gmail.com>
2016-07-08 14:34:37 +0800

    alleviate callback hell & adopt to new API

--- src/main/java/Reporter.java
+++ src/main/java/Reporter.java
@@ -1,7 +1,5 @@
 import io.vertx.core.*;
 import io.vertx.core.http.*;
-import io.vertx.core.impl.verticle.PackageHelper;
-import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
 
 
@@ -13,13 +11,14 @@ import java.util.Base64;
 public class Reporter {
 
   private final static String host = "localhost";
-  private final static String feedURI = "/hawkular/inventory/feeds";
+  private final static String inventoryBaseURI = "/hawkular/inventory/deprecated/";
+  private final static String feedURI = inventoryBaseURI + "feeds/";
   private final static String feedId = "vertx-localhost";
-  private final static String resourceURI = "/hawkular/inventory/feeds/" + feedId + "/resources";
+  private final static String resourceURI =  feedURI + feedId + "/resources/";
   private final static String vertxResourceId = "vertx";
-  private final static String metricTypeURI = "/hawkular/inventory/feeds/" + feedId + "/metricTypes";
+  private final static String metricTypeURI = feedURI + feedId + "/metricTypes/";
   private final static String eventBusResourceId = "event-bus";
-  private final static String metricURI = resourceURI + "/" + eventBusResourceId + "/metrics";
+  private final static String metricURI = resourceURI + eventBusResourceId + "/metrics/";
   private final static String metricId = "registered-handlers";
   private final static String metricTypeId = "something-countable";
   private HttpClient client;
@@ -28,39 +27,68 @@ public class Reporter {
   public static void main(String [] argv) {
     Reporter reporter = new Reporter();
     try {
-      // step 1: create a feed
+      Future<HttpClientResponse> fut1 = Future.future();
+      Future<HttpClientResponse> fut2 = Future.future();
+      Future<HttpClientResponse> fut3 = Future.future();
+      Future<HttpClientResponse> fut4 = Future.future();
+      Future<HttpClientResponse> fut5 = Future.future();
+
       HttpClientRequest request = reporter.getHttpRequest(host, feedURI, HttpMethod.POST).handler(response -> {
-        System.out.println("Created feed!");
-        // step 2: create a vertx resource
+        if (response.statusCode() == 201) {
+          System.out.println("Created feed!");
+          fut1.complete();
+        } else {
+          fut1.fail("Fail when Creating feed");
+        }
+      });
+      JsonObject json = new JsonObject().put("id", feedId);
+      request.end(json.encode());
+
+      fut1.compose(response -> {
+        // step 2: create a vertx
         HttpClientRequest request1 = reporter.getHttpRequest(host, resourceURI, HttpMethod.POST).handler(response1 -> {
           System.out.println("Created vertx resource!");
-          // step 3: create a event bus resource
-          HttpClientRequest request2 = reporter.getHttpRequest(host, resourceURI, HttpMethod.POST).handler(response2 -> {
-            System.out.println("Created event_bus resource!");
-            // step 4: create a metric type
-            HttpClientRequest request3 = reporter.getHttpRequest(host, metricTypeURI, HttpMethod.POST).handler(response3 -> {
-              System.out.println("Created metric type COUNTER!");
-              // step 5: create a metric
-              HttpClientRequest request4 = reporter.getHttpRequest(host, metricURI, HttpMethod.POST).handler(response4 -> {
-                System.out.println("Created metric " + metricId);
-                reporter.vertx.close();
-              });
-              JsonObject json4 = new JsonObject().put("id", metricId).put("metricTypePath", "/mt;" + metricTypeId);
-              request4.end(json4.encode());
-            });
-            JsonObject json3 = new JsonObject().put("id", metricTypeId).put("type", "COUNTER").put("unit", "NONE").put("collectionInterval", "30");
-            request3.end(json3.encode());
-          });
-          JsonObject json2 = new JsonObject().put("id", eventBusResourceId).put("resourceTypePath", "/rt;URL");
-          request2.end(json2.encode());
+          fut2.complete();
         });
         JsonObject json1 = new JsonObject().put("id", vertxResourceId).put("resourceTypePath", "/rt;URL")
                 .put("properties", new JsonObject().put("type", "standalone"));
         request1.end(json1.encode());
+      }, fut2);
+
+      fut2.compose(response -> {
+        // step 3: create a event bus resource
+        HttpClientRequest request2 = reporter.getHttpRequest(host, resourceURI, HttpMethod.POST).handler(response2 -> {
+          System.out.println("Created event_bus resource!");
+          fut3.complete();
+        });
+        JsonObject json2 = new JsonObject().put("id", eventBusResourceId).put("resourceTypePath", "/rt;URL");
+        request2.end(json2.encode());
+      }, fut3);
+
+      fut3.compose(response -> {
+        // step 4: create a metric type
+        HttpClientRequest request3 = reporter.getHttpRequest(host, metricTypeURI, HttpMethod.POST).handler(response3 -> {
+          System.out.println("Created metric type COUNTER!");
+          fut4.complete();
+        });
+        JsonObject json3 = new JsonObject().put("id", metricTypeId).put("type", "COUNTER").put("unit", "NONE").put("collectionInterval", "30");
+        request3.end(json3.encode());
+      }, fut4);
+
+      fut4.compose(response -> {
+        // step 5: create a metric
+        HttpClientRequest request4 = reporter.getHttpRequest(host, metricURI, HttpMethod.POST).handler(response4 -> {
+          System.out.println("Created metric " + metricId);
+          fut5.complete();
+        });
+        JsonObject json4 = new JsonObject().put("id", metricId).put("metricTypePath", "/mt;" + metricTypeId);
+        request4.end(json4.encode());
+      }, fut5);
+
+      fut5.setHandler(httpClientResponseAsyncResult -> {
+        reporter.vertx.close();
       });
 
-      JsonObject json = new JsonObject().put("id", feedId);
-      request.end(json.encode());
     } catch (Exception ex) {
       System.err.println(ex.getLocalizedMessage());
     }
@@ -81,7 +109,10 @@ public class Reporter {
   private HttpClientRequest getHttpRequest(String host, String URI, HttpMethod action) {
     HttpClientOptions options = new HttpClientOptions().setDefaultHost(host).setDefaultPort(8080).setKeepAlive(false);
     if (client == null) client = vertx.createHttpClient(options);
-    HttpClientRequest request = client.request(action, URI);
+    HttpClientRequest request = client.request(action, URI).exceptionHandler(throwable -> {
+      System.err.println(throwable.getLocalizedMessage());
+      vertx.close();
+    });
     addHeaders(request);
     return request;
   }
